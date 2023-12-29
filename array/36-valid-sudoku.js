4a1c032c12a2d105ff6a44765465b082cbbb0ed1
HoaLe69 <hoabanhanh@gmail.com>
2023-12-29 21:26:41 +0700

    problem 36 array lc

--- array/36-valid-sudoku.js
+++ array/36-valid-sudoku.js
@@ -1,4 +1,24 @@
-var isValidSudoku = board => {
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
   const seen = new Set()
-  for (let i = 0; i < board.length; i++) {}
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      // ignore if not number
+      if (board[i][j] === '.') continue
+      const c = board[i][j]
+      if (
+        seen.has(c + 'row' + i) ||
+        seen.has(c + 'col' + j) ||
+        seen.has(c + 'subbox' + Math.floor(i / 3) + Math.floor(j / 3))
+      )
+        return false
+      seen.add(c + 'row' + i)
+      seen.add(c + 'col' + j)
+      seen.add(c + 'subbox' + Math.floor(i / 3) + Math.floor(j / 3))
+    }
+  }
+  return true
 }
